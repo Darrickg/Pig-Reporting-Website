2cf931b15983e790233e61c750071b83737eda02
Darrickg <darrickgunawan@gmail.com>
2022-11-30 02:21:15 -0800

    actually a todo list this time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,20 @@ export class AppComponent {
 }
 
 /*
-  - test
+  TODO: list
+
+  - make a table for the data to be shown
+      - use a mat-table
+
+  - make a sorting for the data in the table
+  
+  - change structure of interface of pig
+      - add pig id
+      - change location
+          - long lat
+      - date and time
+
+  - make form
+      - implement routing
+      - implement add new location
 */
